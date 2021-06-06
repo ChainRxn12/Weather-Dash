7ce721f195e8bbfe7fd5f8a78509b044297017bb
Billy Dorman <b.d.dorman12@gmail.com>
2021-06-05 22:13:39 -0500

    retrieved data from weather api using ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,33 @@ function displayTime() {
   }
 
 setInterval(displayTime, 1000);
+
+// var myApi = '38246d45daca6900eaed0981bae0766c';
+// var currentCity ="";
+// var lastCity = "";
+// var city = $('#searchCity').val();
+// var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + myApi;
+
+$(document).ready(function() {
+  //create event listener for search button
+    $('#search-button').click(function() {
+  //create city variables with button id    
+      var city = $('#searchCity').val();
+      if(city != '') {
+  //retrieve data from open weather api      
+        $.ajax({
+          url:'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=38246d45daca6900eaed0981bae0766c',
+          method: "GET",
+          dataType: "jsonp",
+          success: function(data){
+            console.log(data);
+          }
+        });
+  //display error message when search field 
+      }else {
+        $('#error').html('Field cannot be empty');
+      }
+    });
+});
+
+
