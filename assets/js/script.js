7c98fb53b30e0654de9b6e36a8a7cf5b51ee3920
Billy Dorman <b.d.dorman12@gmail.com>
2021-06-08 12:45:00 -0500

    creating for loop for fiveDayForecast, no display

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,20 +23,19 @@ $(document).ready(function() {
       fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + 
       "&APPID=049ec4543fbcbfe824a5ad0d764bbc6d")
       .then(res => res.json())
-      .then (res => console.log(res))
       .then(data => {
         
-        
+            console.log(data);
             localStorage.setItem('City Name', city);
             var weatherList = currentWeather(data);
             //adds current weather to be displayed on right side of html
             $("#current-weather").html(weatherList);
             //clears out search bar
             $("#search-city").val('')
-            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=049ec4543fbcbfe824a5ad0d764bbc6d`)
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&exclude=minutely,hourly,alerts&appid=049ec4543fbcbfe824a5ad0d764bbc6d`)
             .then(res => res.json())
-            .then (res => console.log(res))
             .then(fiveDayData => {
+              console.log(fiveDayData);
               var fiveDayWeather = weatherForecast(fiveDayData);
               $("#five-day-forecast").html(fiveDayWeather);
             })
@@ -62,9 +61,39 @@ function currentWeather(data) {
 }
 
 function weatherForecast(fiveDayData) {
-  var cards = '';
-  for (i=0; i < fiveDayData.length; i++)
-  return 
+  let fiveDayForecastHTML = `
+  <h2>5-Day Forecast:</h2>
+  <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap ">`;
+  // Loop over the forecast and build the template HTML using UTC offset and Open Weather Map icon
+  for (let i = 0; i < fiveDayData.daily.length; i++) {
+      let dayData = fiveDayData.daily[i];
+      let dayTimeUTC = dayData.dt;
+      let timeZoneOffset = fiveDayData.timezone;
+      let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+      let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+      let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+      // Only displaying mid-day forecasts
+      if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+          fiveDayForecastHTML += `
+          <div class="weather-card card m-2 p0">
+              <ul class="list-unstyled p-3">
+                  <li>${thisMoment.format("MM/DD/YY")}</li>
+                  <li class="weather-icon"><img src="${iconURL}"></li>
+                  <li>Temp: ${dayData.daily.temp}&#8457;</li>
+                  <br>
+                  <li>Humidity: ${dayData.daily.humidity}%</li>
+              </ul>
+          </div>`;
+      }
+      return fiveDayForecastHTML;
+  }
+  // Build the HTML template
+  fiveDayForecastHTML += `</div>`;
+  // Append the five-day forecast to the DOM
+  $('#five-day-forecast').html(fiveDayForecastHTML);
+}
+
+  
   //"<h2><strong>Five Day Weather For</strong>:  "+ fiveDayData.name +" </h2>" ;
         //  "<h3><strong>Temperature</strong>: "+ fiveDayData.daily.temp +"&deg;F</h3>" +
         //  "<h3><strong>Weather</strong>: "+ fiveDayData.daily.weather.main +"</h3>" +
@@ -72,7 +101,9 @@ function weatherForecast(fiveDayData) {
         //  "<h3><strong>Humidity</strong>: "+ fiveDayData.daily.humidity +"%</h3>" +
         //  "<h3><strong>Wind Speed</strong>: "+ fiveDayData.daily.wind_speed +"mph</h3>" +
         //  "<h3><strong>UV Index</strong>: "+ fiveDayData.daily.uvi +"</h3>";
-}
+
+
+
 
 // var currentWeather = () => {
 //   $('#city-results').empty();
