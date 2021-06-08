573cf01903af6c9bf4db59cab2c6c683136303eb
Billy Dorman <b.d.dorman12@gmail.com>
2021-06-07 23:35:16 -0500

    changed ajax to fetch calls, created second api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,36 +16,30 @@ $(document).ready(function() {
   var city = $("#search-city").val();
   
   // if city value is not left blank...
-    if (city != '') { 
-      //create ajax call to openweather API
-      $.ajax({
-        //get url current weather params = city, imperial units, and api key
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + 
-        "&APPID=049ec4543fbcbfe824a5ad0d764bbc6d",
-        method: "GET",
-        dataType: "jsonp",
-        success: function(data){
-          //console log the current weather data to see api response
-          console.log(data);
-          localStorage.setItem('City Name', city);
-          var weatherList = currentWeather(data);
-          //adds current weather to be displayed on right side of html
-          $("#current-weather").html(weatherList);
-          //clears out search bar
-          $("#search-city").val('');
-        }
-      });
-      $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&APPID=049ec4543fbcbfe824a5ad0d764bbc6d",
-        method: "GET",
-        dataType: "jsonp",
-        success: function(response) {
-          console.log(response);
-          var fiveDayList = fiveDayForecast(response);
-          $("#five-day-forecast").html(fiveDayList);
-        }
-
-      });
+    if (city !== '') { 
+      fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + 
+      "&APPID=049ec4543fbcbfe824a5ad0d764bbc6d")
+      .then(res => res.json())
+      
+      .then(data => {
+        
+        
+            localStorage.setItem('City Name', city);
+            var weatherList = currentWeather(data);
+            //adds current weather to be displayed on right side of html
+            $("#current-weather").html(weatherList);
+            //clears out search bar
+            $("#search-city").val('')
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=049ec4543fbcbfe824a5ad0d764bbc6d`)
+            .then(res => res.json())
+            
+            .then(fiveDayData => {
+              var fiveDayWeather = weatherForecast(fiveDayData);
+              $("#five-day-forecast").html(fiveDayWeather);
+            })
+        })
+      
+   
       //error message with an empty search
     }else{
       $("#error").html('Please Enter a City Name');
@@ -64,14 +58,14 @@ function currentWeather(data) {
          "<h3><strong>UV Index</strong>: "+ data.weather[0].main +"</h3>";
 }
 
-function weatherForecast(response) {
-  return "<h2><strong>Five Day Weather For</strong>: "+ data.name +" </h2>" +
-         "<h3><strong>Temperature</strong>: "+ data.list.main.temp +"&deg;F</h3>" +
-         "<h3><strong>Weather</strong>: "+ data.list.weather.main +"</h3>" +
-         "<h3><strong>Description</strong>: "+ data.list.weather.description +"</h3>" +
-         "<h3><strong>Humidity</strong>: "+ data.list.main.humidity +"%</h3>" +
-         "<h3><strong>Wind Speed</strong>: "+ data.list.wind.speed +"mph</h3>";
-         //"<h3><strong>UV Index</strong>: "+ data.weather[0].main +"</h3>";
+function weatherForecast(fiveDayData) {
+  return "<h2><strong>Five Day Weather For</strong>:  "+ fiveDayData.name +" </h2>" ;
+        //  "<h3><strong>Temperature</strong>: "+ fiveDayData.daily.temp +"&deg;F</h3>" +
+        //  "<h3><strong>Weather</strong>: "+ fiveDayData.daily.weather.main +"</h3>" +
+        //  "<h3><strong>Description</strong>: "+ fiveDayData.daily.weather.description +"</h3>" +
+        //  "<h3><strong>Humidity</strong>: "+ fiveDayData.daily.humidity +"%</h3>" +
+        //  "<h3><strong>Wind Speed</strong>: "+ fiveDayData.daily.wind_speed +"mph</h3>" +
+        //  "<h3><strong>UV Index</strong>: "+ fiveDayData.daily.uvi +"</h3>";
 }
 
 //var latLong = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city , &state, &country + '&appid=049ec4543fbcbfe824a5ad0d764bbc6d',
