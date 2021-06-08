bbaec62195c97b0d00ab979592a6e04a9a349140
Billy Dorman <b.d.dorman12@gmail.com>
2021-06-08 07:58:29 -0500

    working on past weather searches appending to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,9 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
+var currentCity = "";
+var lastCity = "";
+
 // ready DOM
 $(document).ready(function() {
 // create event listener for form submit button
@@ -67,6 +70,56 @@ function weatherForecast(fiveDayData) {
         //  "<h3><strong>Wind Speed</strong>: "+ fiveDayData.daily.wind_speed +"mph</h3>" +
         //  "<h3><strong>UV Index</strong>: "+ fiveDayData.daily.uvi +"</h3>";
 }
+
+// var currentWeather = () => {
+//   $('#city-results').empty();
+//   // If localStorage is empty
+//   if (localStorage.length===0){
+//       if (lastCity){
+//           $('#search-city').attr("value", lastCity);
+//       } else {
+//           $('#search-city').attr("value", "Austin");
+//       }
+//   } else {
+//       // Build key of last city written to localStorage
+//       let lastCityKey="cities"+(localStorage.length-1);
+//       lastCity=localStorage.getItem(lastCityKey);
+//       // Set search input to last city searched
+//       $('#search-city').attr("value", lastCity);
+//       // Append stored cities to page
+//       for (let i = 0; i < localStorage.length; i++) {
+//           let city = localStorage.getItem("cities" + i);
+//           let cityEl;
+//           // Set to lastCity if currentCity not set
+//           if (currentCity===""){
+//               currentCity=lastCity;
+//           }
+//           // Set button class to active for currentCity
+//           if (city === currentCity) {
+//               cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
+//           } else {
+//               cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
+//           } 
+//           // Append city to page
+//           $('#city-results').prepend(cityEl);
+//       }
+//       // Add a "clear" button to page if there is a cities list
+//       if (localStorage.length>0){
+//           $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
+//       } else {
+//           $('#clear-storage').html('');
+//       }
+//   }
+  
+// }
+
+// // Old searched cities buttons event listener
+// $('#city-results').on("click", (event) => {
+//   event.preventDefault();
+//   $('#search-city').val(event.target.textContent);
+//   currentCity=$('#search-city').val();
+//   currentWeather(event);
+// });
 
 //var latLong = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city , &state, &country + '&appid=049ec4543fbcbfe824a5ad0d764bbc6d',
 
