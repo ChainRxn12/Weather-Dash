56e9dd760819c0f2063439b67ae162eae4d9b374
Billy Dorman <b.d.dorman12@gmail.com>
2021-06-07 22:01:05 -0500

    debugging second ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,6 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
-//var myApi = '38246d45daca6900eaed0981bae0766c';
-// var currentCity ="";
-// var lastCity = "";
-// var searchButton = $('#search-button');
-
 // ready DOM
 $(document).ready(function() {
 // create event listener for form submit button
@@ -27,7 +22,7 @@ $(document).ready(function() {
         //get url current weather params = city, imperial units, and api key
         url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + 
         "&APPID=049ec4543fbcbfe824a5ad0d764bbc6d",
-        type: "GET",
+        method: "GET",
         dataType: "jsonp",
         success: function(data){
           //console log the current weather data to see api response
@@ -38,11 +33,18 @@ $(document).ready(function() {
           $("#current-weather").html(weatherList);
           //clears out search bar
           $("#search-city").val('');
-          //run second ajax call to openweather API
-          //  $.ajax({
-          //    url: 'https://api.openweathermap.org/data/2.5/onecall?' "&lat="
-          
         }
+      });
+      $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&APPID=049ec4543fbcbfe824a5ad0d764bbc6d",
+        method: "GET",
+        dataType: "jsonp",
+        success: function(response) {
+          console.log(response);
+          var fiveDayList = fiveDayForecast(response);
+          $("#five-day-forecast").html(fiveDayList);
+        }
+
       });
       //error message with an empty search
     }else{
@@ -62,14 +64,15 @@ function currentWeather(data) {
          "<h3><strong>UV Index</strong>: "+ data.weather[0].main +"</h3>";
 }
 
-// function weatherForecast(data) {
-//   return "<h2><strong>Five Day Weather For</strong>: "+ data.name +" </h2>" +
-//          "<h3><strong>Temperature</strong>: "+ data.list.main.temp +"&deg;F</h3>" +
-//          "<h3><strong>Weather</strong>: "+ data.list.weather.main +"</h3>" +
-//          "<h3><strong>Description</strong>: "+ data.list.weather.description +"</h3>" +
-//          "<h3><strong>Humidity</strong>: "+ data.list.main.humidity +"%</h3>" +
-//          "<h3><strong>Wind Speed</strong>: "+ data.list.wind.speed +"mph</h3>";
-//          //"<h3><strong>UV Index</strong>: "+ data.weather[0].main +"</h3>";
-// }
+function weatherForecast(response) {
+  return "<h2><strong>Five Day Weather For</strong>: "+ data.name +" </h2>" +
+         "<h3><strong>Temperature</strong>: "+ data.list.main.temp +"&deg;F</h3>" +
+         "<h3><strong>Weather</strong>: "+ data.list.weather.main +"</h3>" +
+         "<h3><strong>Description</strong>: "+ data.list.weather.description +"</h3>" +
+         "<h3><strong>Humidity</strong>: "+ data.list.main.humidity +"%</h3>" +
+         "<h3><strong>Wind Speed</strong>: "+ data.list.wind.speed +"mph</h3>";
+         //"<h3><strong>UV Index</strong>: "+ data.weather[0].main +"</h3>";
+}
 
 //var latLong = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city , &state, &country + '&appid=049ec4543fbcbfe824a5ad0d764bbc6d',
+
